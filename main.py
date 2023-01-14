c19c51f7cac3cce62b23c5917c37893aa38c4155
FrnRmn <francescoromandini@gmail.com>
2023-01-14 17:33:47 +0100

    dynamic text scraping try

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-print("BRELLALLONI")
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
-print("ancora")
+# Define url of interest (Italy)
+url_electricity_map = "https://app.electricitymaps.com/zone/IT?aggregated=true"
+
+# Set driver of Chrome (required version > 9.0)
+driver = webdriver.Chrome('./chromedriver') 
+driver.get(url_electricity_map) 
+time.sleep(3) #give time for dynamic text to load
+html = driver.page_source
+
+# Find carbon intensity data on left panel
+soup = BeautifulSoup(html, "html.parser")
+carbon_intensity_square = soup.find_all("span", class_="carbonintensitysquare__Value-llslJL hNchmo")
+carbon_intensity_value = carbon_intensity_square[0].text
+print("Italy Carbon Intensity: {0}g (gCO₂eq/kWh)".format(carbon_intensity_value))
