690d77ebfa564c26087e1928b613d186aed0da08
giulianogiari <giuliano.giari@gmail.com>
2023-01-31 19:50:25 +0100

    add chrome driver

--- src/energymonitor/scraping.py
+++ src/energymonitor/scraping.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+
 def browser_setup(browser_name):
-    '''Inizialize webdriver paramiters for a specific browser
+    """
+    Inizialize webdriver paramiters for a specific browser
 
     Parameters
     ----------
@@ -13,32 +15,39 @@ def browser_setup(browser_name):
     -------
     driver: selenium.webdriver
         webdriver object with paramenters for the chosen browser.
+    """
 
-    '''
     if browser_name == 'Chrome':
         # Set driver of Chrome (requires version > 9.0)
         options = webdriver.ChromeOptions()
+        # import chrome driver
+        from webdriver_manager.chrome import ChromeDriverManager
         # Disable popup
         options.add_argument("--headless")
-        driver = webdriver.Chrome(options=options)
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     elif browser_name == 'Firefox':
+        # todo: this is currently not working on mac
         # Set driver for Firefox
         options = webdriver.FirefoxOptions()
-        #Disable popup
+        # import firefox driver
+        from webdriver_manager.firefox import GeckoDriverManager
+        # Disable popup
         options.add_argument("--headless")
-        driver = webdriver.Firefox(options=options)
+        driver = webdriver.Firefox(GeckoDriverManager().install(), options=options)
 
     return driver
 
+
 def get_carbon_intensity(html):
-    '''
+    """
     Get the carbon intensity value of a certain country from the left panel of Electricity Map
 
     :param html: The html content requested with selenium
     :type html: str
     :return: The value representing the current carbon intensity for a given country
     :rtype: str
-    '''
+    """
+
     soup = BeautifulSoup(html, "html.parser")
     carbon_intensity_square = soup.find_all("p", attrs={"data-test-id": "co2-square-value"})
     carbon_intensity_value = carbon_intensity_square[0].text
