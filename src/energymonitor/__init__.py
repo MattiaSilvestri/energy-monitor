0dead3ef80e19d7409f82b9a18a7c639778b38f9
FrnRmn <francescoromandini@gmail.com>
2023-02-17 20:23:40 +0100

    add general get request

--- src/energymonitor/__init__.py
+++ src/energymonitor/__init__.py
@@ -1,3 +1,4 @@
 from energymonitor.scraping import chrome_browser_setup
 from energymonitor.scraping import get_carbon_intensity
 from energymonitor.cpu_usage import get_cpu_info
+from energymonitor.co2api import get_request_co2signal
