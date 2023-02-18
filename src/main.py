66cc27fa6488f6f8554da03a4d3018613487616c
FrnRmn <francescoromandini@gmail.com>
2023-02-18 17:36:45 +0100

    print format update

--- src/main.py
+++ src/main.py
@@ -5,16 +5,16 @@ country_code = "IT"
 
 
 # Retrieve data from Co2Signal API
-print('Retrieving Co2Signal data...')
+print('\nRetrieving Co2Signal data...')
 co2data = energymonitor.get_request_co2signal(country_code)
 
 # Extract relevant measures
 carbon_intensity = co2data['data']['carbonIntensity']
 carbon_intensity_unit = co2data['units']['carbonIntensity']
-fossil_percentage = co2data['data']['fossilFuelPercentage']
-print("\n Selected Country: {0} \n Carbon Intensity: {1} ({2}) \n Percentage of fossil fuel: {3}%".format(country_code, carbon_intensity, carbon_intensity_unit, fossil_percentage))
+fossil_percentage = round(co2data['data']['fossilFuelPercentage'],2)
+print("- Selected Country: {0} \n- Carbon Intensity: {1} ({2}) \n- Percentage of fossil fuel: {3}%".format(country_code, carbon_intensity, carbon_intensity_unit, fossil_percentage))
 
 # Retrieve cpu info
-print('Retrieving CPU data...')
+print('\nRetrieving CPU data...')
 cpu_info = energymonitor.get_cpu_info()
-print("\n Current CPU: {0}".format(cpu_info))
+print("- Current CPU: {0}".format(cpu_info))
