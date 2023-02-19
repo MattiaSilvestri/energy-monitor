fd4b93280a6fd4acb274678837b5e0cbdcb7127a
FrnRmn <francescoromandini@gmail.com>
2023-02-19 12:11:45 +0100

    add cpu usage retrieval

--- src/energymonitor/cpu_usage.py
+++ src/energymonitor/cpu_usage.py
@@ -1,10 +1,11 @@
 import cpuinfo
+import psutil
 
 def get_cpu_info():
     """
-    Return info regarding CPU model installed
+    Return info regarding CPU model installed.
 
-    :return: Info regarding CPU model installed.
+    :return: Info regarding CPU model installed
     :rtype: list
     """
 
@@ -14,3 +15,21 @@ def get_cpu_info():
         cpu_info= "CPU Info not available"
         
     return [cpu_info]
+
+
+def get_cpu_usage(seconds):
+    """
+    Return the average CPU usage over a time interval period.
+
+    :param seconds: Time interval period considered
+    :type seconds: float
+    :return: CPU usage percentage
+    :rtype: float
+    """
+
+    try:
+        cpu_percentage = psutil.cpu_percent(seconds)
+    except:
+        cpu_percentage= "CPU usage not available"
+        
+    return cpu_percentage
