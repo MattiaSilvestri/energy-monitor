97938d31c6c1c9d557d00588acd921d21277f1cc
FrnRmn <francescoromandini@gmail.com>
2023-04-29 20:08:24 +0200

    integrate gui with cpuCO2 module

--- src/core/gui.py
+++ src/core/gui.py
@@ -9,27 +9,29 @@ from random import randint
 
 
 class MyApp(QWidget):
-    def __init__(self):
+    def __init__(self, cpu_tdp, co2_intensity, get_interval_emissions_method):
         super().__init__()
         self.setWindowTitle('Energy Monitor Plot')
         # self.window_width, self.window_height = 1200, 800
         # self.setMinimumSize(self.window_width, self.window_height)
+        self.get_interval_emissions_method = get_interval_emissions_method
 
         layout = QVBoxLayout()
         self.setLayout(layout)
 
         self.canvas = FigureCanvas(plt.Figure(figsize=(15, 3)))
         layout.addWidget(self.canvas)
-
         self.insert_ax()
 
         # some example data
-        self.x = np.arange(200)
+        self.x = np.arange(200)[::-1]*-1
         self.y = self.x*0
+        self.cpu_tdp = cpu_tdp
+        self.co2_intesity = co2_intensity
 
         # timer
         self.timer = QTimer()
-        self.timer.setInterval(100)
+        self.timer.setInterval(1000)
         self.timer.timeout.connect(self.update_chart)
         self.timer.start()
 
@@ -40,12 +42,14 @@ class MyApp(QWidget):
         }
         matplotlib.rc('font', **font)
         self.ax = self.canvas.figure.subplots()
-        self.ax.set_xlim([0, 200])
+        self.ax.set_xlim([-200, 0])
         self.line_plot = None
 
     def update_chart(self):
+
+        new_value =  self.get_interval_emissions_method(self.cpu_tdp, self.co2_intesity)  # randint(0,50)
         self.y = self.y[1:]  # Remove the first
-        self.y = np.append(self.y, randint(0,50))  # Add a new random value.
+        self.y = np.append(self.y, new_value)
 
         if self.line_plot:
             self.line_plot[0].remove()
