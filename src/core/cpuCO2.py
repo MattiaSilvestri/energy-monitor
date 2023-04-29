493f9aea208fb11d00e65ce2139f3938043dd6a6
FrnRmn <francescoromandini@gmail.com>
2023-04-29 20:08:24 +0200

    integrate gui with cpuCO2 module

--- src/core/cpuCO2.py
+++ src/core/cpuCO2.py
@@ -1,4 +1,7 @@
 from utils import api_calls, cpu
+from PyQt5.QtWidgets import QApplication
+import sys
+from core.gui import MyApp
 
 def cpu_co2(country_code = None) -> None:
     """Function to run the core of the app: compare CPU usage with CO2 emissions"""
@@ -26,7 +29,7 @@ def cpu_co2(country_code = None) -> None:
         print("- Selected Country: {0} \n- Carbon Intensity: {1} ({2}) \n- Percentage of fossil fuel: {3}%".format(country_code, carbon_intensity, carbon_intensity_unit, fossil_percentage))
 
         # Retrieve cpu info
-        cpu_retrieval_time = 10 #to export in a configuration file
+        cpu_retrieval_time = 2 #to export in a configuration file
         print(f'\nRetrieving CPU usage of the next {cpu_retrieval_time} seconds...')
         cpu_info = cpu.get_cpu_info()
         cpu_usage = cpu.get_cpu_usage(cpu_retrieval_time) #it seems too low to be true!
@@ -36,6 +39,22 @@ def cpu_co2(country_code = None) -> None:
         print('\nCombining Co2 and CPU data...')
         co2_emissions = combine_cpu_CO2(cpu_usage, cpu.get_cpu_tdp(cpu_info), carbon_intensity)
         print("- In the last {0} seconds your CPU footprint was: {1} g".format(cpu_retrieval_time, co2_emissions))
+
+        # Launch plot
+        app = QApplication(sys.argv)
+        app.setStyleSheet('''
+            QWidget {
+                font-size: 30px;
+            }
+        ''')
+        myApp = MyApp(cpu.get_cpu_tdp(), carbon_intensity, get_interval_emissions)
+        myApp.show()
+        print('\nPlot window is execution...')
+        try:
+            sys.exit(app.exec_())
+        except SystemExit:
+            print('Closing Window...')
+
 
     except KeyError:
         print('Sorry, your country is not present in the Electricity Map ' +
@@ -54,11 +73,30 @@ def combine_cpu_CO2(cpu_usage: float, cpu_tdp: int, co2_intensity: float) -> flo
     :type co2_intensity: float
     :return: grams of CO2 emitted for your CPU usage in the specified time
     :rtype: float
-
-    :return: CPU usage percentage
-    :rtype: float or None
     """
     cpu_tdp_KWh = cpu_tdp/1000
     used_KWh = cpu_usage * cpu_tdp_KWh / 100
     co2_emissions = round(used_KWh * co2_intensity, 2)
+    return co2_emissions
+
+
+def get_interval_emissions(cpu_tdp: int, co2_intensity: float) -> float:
+    """
+    From a given cpu_tdp and co2_intensity return the co2 emissions computing the cpu_usage at current time.
+
+    :param cpu_tdp: CPU Thermal Design Power (TDP) in watts
+    :type cpu_tdp: int
+    :param co2_intensity: carbon intensity value for the selected country
+    :type co2_intensity: float
+    :return: grams of CO2 emitted for your CPU usage in the specified time
+    :rtype: float
+    """
+
+    # Retrieve cpu usage
+    cpu_retrieval_time = 1 #to export in a configuration file
+    cpu_usage = cpu.get_cpu_usage(cpu_retrieval_time)
+
+    # Combine Co2 with CPU usage
+    co2_emissions = combine_cpu_CO2(cpu_usage, cpu_tdp, co2_intensity)
+
     return co2_emissions
