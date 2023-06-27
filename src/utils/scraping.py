dedb5c61d33c56b17db30e34245e7416047bcef9
giulianogiari <giuliano.giari@gmail.com>
2023-06-27 19:58:46 +0200

    skip type check for webdriver

--- src/utils/scraping.py
+++ src/utils/scraping.py
@@ -6,7 +6,7 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager # type: ignore
 
 def chrome_browser_setup():
     """
