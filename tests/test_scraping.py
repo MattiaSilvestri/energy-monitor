9bbcf7d4d27cb629d8cb9b47007e1053ead14eb6
giulianogiari <giuliano.giari@gmail.com>
2023-09-12 19:35:01 +0200

    fix

--- tests/test_scraping.py
+++ tests/test_scraping.py
@@ -7,16 +7,15 @@ from utils.scraping import get_cpu_database
     
 
 def test_get_cpu_database():
-    # 
-    assert isinstance(get_cpu_database('AMD Ryzen 7 5700U with Radeon Graphics'), dict)
     # check that a json file has been saved in the data folder
     path = os.path.abspath(os.path.join(os.path.dirname(__name__), 'data'))
     json_fname = os.path.join(path, 'database_amd.json')
+    assert isinstance(get_cpu_database(json_fname, 'AMD'), dict)
     assert os.path.isfile(json_fname)
 
-    assert isinstance(get_cpu_database('Intel(R) Core(TM) i7-8569U CPU @ 2.80GHz'), dict)
-    # check that a json file has been saved in the data folder
+    # repeat for intel
     path = os.path.abspath(os.path.join(os.path.dirname(__name__), 'data'))
     json_fname = os.path.join(path, 'database_intel.json')
+    assert isinstance(get_cpu_database(json_fname, 'intel'), dict)
     assert os.path.isfile(json_fname)
 
