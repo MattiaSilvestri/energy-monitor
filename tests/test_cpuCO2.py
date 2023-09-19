f1b57851fb5a64f63e2822af3eb2b54a09507904
giulianogiari <giuliano.giari@gmail.com>
2023-09-19 19:09:04 +0200

    emissions >= 0

--- tests/test_cpuCO2.py
+++ tests/test_cpuCO2.py
@@ -2,7 +2,7 @@ import os
 import sys
 import pytest
 # add src to path to be able to run the tests
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__name__), 'src')))
+sys.path.append(os.path.join(os.path.dirname(__file__).split("energy-monitor")[0], 'energy-monitor', 'src'))
 from core.cpuCO2 import combine_cpu_CO2, get_interval_emissions
 
 
@@ -17,4 +17,4 @@ def test_get_interval_emissions():
     # test that the function returns a float
     assert isinstance(get_interval_emissions(1.0,1,1), float)
     # test that the function returns a number larger than 0
-    assert get_interval_emissions(15., 490, 1) > 0
+    assert get_interval_emissions(28., 500, 1) >= 0
