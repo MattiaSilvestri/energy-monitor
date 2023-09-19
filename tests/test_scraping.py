4f2299cdc38aae2dc2ddee037b76182517f0d08b
giulianogiari <giuliano.giari@gmail.com>
2023-09-19 18:34:52 +0200

    update path

--- tests/test_scraping.py
+++ tests/test_scraping.py
@@ -2,20 +2,17 @@ import os
 import sys
 import pytest
 # add src to path to be able to run the tests
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__name__), 'src')))
+sys.path.append(os.path.join(os.path.dirname(__file__).split('tests')[0], 'src'))
 from utils.scraping import get_cpu_database
     
 
 def test_get_cpu_database():
     # check that a json file has been saved in the data folder
-    path = os.path.abspath(os.path.join(os.path.dirname(__name__), 'data'))
-    json_fname = os.path.join(path, 'database_amd.json')
+    json_fname = os.path.join(os.path.dirname(__file__).split("tests")[0], 'data', 'database_amd.json')
     assert isinstance(get_cpu_database(json_fname, 'AMD'), dict)
     assert os.path.isfile(json_fname)
 
     # repeat for intel
-    path = os.path.abspath(os.path.join(os.path.dirname(__name__), 'data'))
-    json_fname = os.path.join(path, 'database_intel.json')
+    json_fname = os.path.join(os.path.dirname(__file__).split("tests")[0], 'data', 'database_intel.json')
     assert isinstance(get_cpu_database(json_fname, 'intel'), dict)
     assert os.path.isfile(json_fname)
-
