132ea9e437e1c97aabb53e50dc2a1f183ac19f58
giulianogiari <giuliano.giari@gmail.com>
2023-09-19 19:48:33 +0200

    return to previous path

--- tests/test_cpu.py
+++ tests/test_cpu.py
@@ -2,7 +2,7 @@ import os
 import sys
 import pytest
 # add src to path to be able to run the tests
-sys.path.append(os.path.join(os.path.dirname(__file__).split("energy-monitor")[0], 'energy-monitor', 'src'))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__name__), 'src')))
 from utils.cpu import get_cpu_info, get_cpu_usage, get_cpu_tdp
 
 
