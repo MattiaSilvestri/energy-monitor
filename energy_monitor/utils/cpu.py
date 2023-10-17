e4585e6b5a070ecbe00accee9beaa58efe83fcbd
MattiaSilvestri <mattiasilvestri27@gmail.com>
2023-10-17 18:52:30 +0200

    update .gitignore

--- energy_monitor/utils/cpu.py
+++ energy_monitor/utils/cpu.py
@@ -1,8 +1,10 @@
-import cpuinfo # type: ignore
+import cpuinfo  # type: ignore
 import json
 import os
 import psutil
 import re
+import sys
+
 from energy_monitor.utils.scraping import scrape_tdp_intel, get_AMD_database
 
 
@@ -16,8 +18,8 @@ def get_cpu_info() -> str:
     try:
         cpu_info = cpuinfo.get_cpu_info()["brand_raw"]
     except:
-        cpu_info= "CPU Info not available"
-        
+        cpu_info = "CPU Info not available"
+
     return cpu_info
 
 
@@ -28,10 +30,10 @@ def get_cpu_usage(seconds: float) -> float:
     :param seconds: Time interval period considered
     :type seconds: float
     :return: CPU usage percentage
-    :rtype: float 
+    :rtype: float
     """
     cpu_percentage = psutil.cpu_percent(seconds)
-        
+
     return cpu_percentage
 
 
@@ -44,20 +46,23 @@ def get_cpu_tdp(cpu_name: str) -> float:
     :rtype: float
     """
     # get the path to the data folder
-    path = [x[0] for x in os.walk('..') if 'data' in x[0]][0]
+    # path = [x[0] for x in os.walk("..") if "data" in x[0]][0]
+    path = os.path.join(
+        os.path.dirname(os.path.dirname(__file__)).split("utils")[0], "data"
+    )
     # define file name of the output file
-    json_fname = os.path.join(path, 'cpu_tdp.json')
-    if os.path.isfile(json_fname) and not 'pytest' in sys.modules:
+    json_fname = os.path.join(path, "cpu_tdp.json")
+    if os.path.isfile(json_fname) and not "pytest" in sys.modules:
         # load json file with the cpu tdp info
-        with open(json_fname, 'r') as f:
-            tdp = json.load(f)['tdp']
-    else: 
-        if 'Intel' in cpu_name:
+        with open(json_fname, "r") as f:
+            tdp = json.load(f)["tdp"]
+    else:
+        if "Intel" in cpu_name:
             # retrieve information from the intel website
             tdp = scrape_tdp_intel(cpu_name)
-        elif 'AMD' in cpu_name:
+        elif "AMD" in cpu_name:
             # use the json file stored in the data folder to retrieve the TDP
-            amd_fname = os.path.join(path, 'tableExport.json')
+            amd_fname = os.path.join(path, "tableExport.json")
             if not os.path.isfile(amd_fname):
                 # download it from our github repository
                 data = get_AMD_database(amd_fname)
@@ -65,34 +70,42 @@ def get_cpu_tdp(cpu_name: str) -> float:
                 # returns JSON object as a dictionary
                 data = json.load(open(amd_fname))
             # retrieve the model code (identified by digits and alphabetic character) from cpu_name
-            my_model_code = re.findall(r'\d+\w+', cpu_name)[0]
+            my_model_code = re.findall(r"\d+\w+", cpu_name)[0]
             # initialize the list of TDPs. this is to account for multiple potential matches
             tdp = []
             # Iterate through the json to get the model name and the TDP
-            for model_info in data['data']:
+            for model_info in data["data"]:
                 # retrieve the model code (identified by digits and alphabetic character) from cpu_name
-                this_model_code = re.findall(r'\d+\w+', str(model_info['Model']))[0]
+                this_model_code = re.findall(r"\d+\w+", str(model_info["Model"]))[0]
                 # check that the model code of this iteration matches the one of the CPU of the user
                 if this_model_code == my_model_code:
                     # get the TDP of the CPU
-                    model_tdp = model_info['Default TDP']
+                    model_tdp = model_info["Default TDP"]
                     # check if its empty
-                    if model_tdp == '':
+                    if model_tdp == "":
                         # e.g., model_info['Model'] = AMD Ryzen 7 7736U
-                        raise ValueError(f"TDP not found for model {model_info['Model']}")
+                        raise ValueError(
+                            f"TDP not found for model {model_info['Model']}"
+                        )
                     # split model tdp string by any non-numeric character, remove empty strings and convert to float
-                    model_tdp =  [float(x) for x in list(filter(None, re.split(r'\D+', model_tdp)))]
+                    model_tdp = [
+                        float(x)
+                        for x in list(filter(None, re.split(r"\D+", model_tdp)))
+                    ]
                     if len(model_tdp) > 1:
                         # average the TDPs
                         tdp.append(sum(model_tdp) / len(model_tdp))
                     else:
                         tdp.append(model_tdp[0])
-            assert len(tdp) == 1, 'Found multiple matching TDPs'
+            assert len(tdp) == 1, "Found multiple matching TDPs"
             tdp = tdp[0]
         else:
-            raise ValueError('CPU not supported')
+            raise ValueError("CPU not supported")
         # save the tdp in a json file
-        with open(json_fname, 'w') as f:
-            json.dump({'tdp': tdp}, f)
+        with open(json_fname, "w") as f:
+            json.dump({"tdp": tdp}, f)
     # return the tdp value
     return tdp
+
+
+# get_cpu_tdp("Intel(R) Core(TM) i7-10700K CPU @ 3.80GHz")
--- energy_monitor/utils/scraping.py
+++ energy_monitor/utils/scraping.py
@@ -7,7 +7,7 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager # type: ignore
+from webdriver_manager.chrome import ChromeDriverManager  # type: ignore
 
 
 def chrome_browser_setup():
@@ -21,7 +21,7 @@ def chrome_browser_setup():
     # Set driver of Chrome (requires version > 9.0)
     options = webdriver.ChromeOptions()
     # Specify verbosity level 0:info, 1:warnings, 2:error, 3:fatal
-    options.add_argument('log-level=1')    
+    options.add_argument("log-level=1")
     # Disable popup
     options.add_argument("--headless")
 
@@ -40,18 +40,20 @@ def scrape_tdp_intel(cpu_name) -> float:
     # initialize the webdriver
     driver = chrome_browser_setup()
     # open the intel website with selenium
-    search_url = "https://ark.intel.com/content/www/us/en/ark/search.html?_charset_=UTF-8&q="
+    search_url = (
+        "https://ark.intel.com/content/www/us/en/ark/search.html?_charset_=UTF-8&q="
+    )
     driver.get(search_url)
     # get the search bar
     search_bar = driver.find_element(By.ID, "ark-searchbox")
     # send the cpu_name to the search bar on the website
-    search_bar.send_keys([x for x in cpu_name.split(' ') if x.startswith('i')][0])
+    search_bar.send_keys([x for x in cpu_name.split(" ") if x.startswith("i")][0])
     # search for the name that was sent
     search_bar.send_keys(Keys.RETURN)
     # find the span element that contains the TDP, identified by the attribute "MaxTDP"
     tdp = driver.find_element(By.XPATH, "//span[@data-key='MaxTDP']").text
     # retain only the numbers including the decimal points, if present
-    tdp = float(re.findall(r'\d+\.\d+|\d+', tdp)[0])
+    tdp = float(re.findall(r"\d+\.\d+|\d+", tdp)[0])
     # close the driver
     driver.close()
     # return the tdp
@@ -66,11 +68,11 @@ def get_AMD_database(fname) -> dict:
     :return: dictionary containing the AMD database
     """
     # url of the json file in our github repository
-    # todo: update according to branch 
-    url = 'https://raw.githubusercontent.com/MattiaSilvestri/energy-monitor/cpu_data/data/tableExport.json'
+    # todo: update according to branch
+    url = "https://raw.githubusercontent.com/MattiaSilvestri/energy-monitor/cpu_data/data/tableExport.json"
     # get the json file in the url using requests and parse it as json format
     data = requests.get(url).json()
     # save it in the data folder
-    with open(fname, 'w') as f:
+    with open(fname, "w") as f:
         json.dump(data, f)
     return data
