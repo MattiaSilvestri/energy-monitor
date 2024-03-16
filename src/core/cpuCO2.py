37c88b76d7c6008dd46f2e9f503584fab806f122
FrnRmn <francescoromandini@gmail.com>
2024-03-16 10:38:43 +0100

    force exit

--- src/core/cpuCO2.py
+++ src/core/cpuCO2.py
@@ -3,13 +3,14 @@ from PyQt5.QtWidgets import QApplication
 import sys
 from core.gui import PlotWindowApp
 from utils.io import safe_read_config
+import os
 
 # Read YAML file
-config = safe_read_config('config.yml')
+config = safe_read_config("config.yml")
 config_appearence = config["Appearance"]
 
 
-def cpu_co2(country_code = None) -> None:
+def cpu_co2(country_code=None) -> None:
     """Function to run the core of the app: compare CPU usage with CO2 emissions"""
 
     # Use manual country code if set, otherwise use geolocalizaion
@@ -19,27 +20,34 @@ def cpu_co2(country_code = None) -> None:
     else:
         # Get location
         geolocation = api_calls.get_location()
-        lon = geolocation['longitude']
-        lat = geolocation['latitude']
+        lon = geolocation["longitude"]
+        lat = geolocation["latitude"]
 
         # Retrieve data from Co2Signal API
-        try: 
+        try:
             co2data = api_calls.get_request_co2signal(lon=lon, lat=lat)
         except Exception as e:
-            print("\nco2data for your location could not be retrieved."
-                "\nPlease try again later or specify a country code")
-        country_code = co2data['countryCode']
+            print(
+                "\nco2data for your location could not be retrieved."
+                "\nPlease try again later or specify a country code"
+            )
+            os._exit(1)
+        country_code = co2data["countryCode"]
 
     try:
         # Extract relevant measures
-        print('\nRetrieving Co2Signal data...')
-        carbon_intensity = co2data['data']['carbonIntensity']
-        carbon_intensity_unit = co2data['units']['carbonIntensity']
-        fossil_percentage = round(co2data['data']['fossilFuelPercentage'],2)
-        print("- Selected Country: {0} \n- Carbon Intensity: {1} ({2}) \n- Percentage of fossil fuel: {3}%".format(country_code, carbon_intensity, carbon_intensity_unit, fossil_percentage))
+        print("\nRetrieving Co2Signal data...")
+        carbon_intensity = co2data["data"]["carbonIntensity"]
+        carbon_intensity_unit = co2data["units"]["carbonIntensity"]
+        fossil_percentage = round(co2data["data"]["fossilFuelPercentage"], 2)
+        print(
+            "- Selected Country: {0} \n- Carbon Intensity: {1} ({2}) \n- Percentage of fossil fuel: {3}%".format(
+                country_code, carbon_intensity, carbon_intensity_unit, fossil_percentage
+            )
+        )
 
         # Retrieve cpu info
-        print('\nRetrieving CPU data...')
+        print("\nRetrieving CPU data...")
         cpu_retrieval_time = config["Functional"]["cpu_retrieval_time"]
         # print(config_file.Functional.cpu_retrieval_time)
         cpu_info = cpu.get_cpu_info()
@@ -47,29 +55,39 @@ def cpu_co2(country_code = None) -> None:
 
         # Launch plot
         app = QApplication(sys.argv)
-        qwidget_params = '''
+        qwidget_params = """
             QWidget {{
                 font-size: {int}px;
             }}
-        '''.format(int=config_appearence['Window']["window_font_size"])
+        """.format(
+            int=config_appearence["Window"]["window_font_size"]
+        )
         app.setStyleSheet(qwidget_params)
-        myApp = PlotWindowApp(cpu.get_cpu_tdp(cpu_info), carbon_intensity, cpu_retrieval_time, get_interval_emissions, config_appearence)
+        myApp = PlotWindowApp(
+            cpu.get_cpu_tdp(cpu_info),
+            carbon_intensity,
+            cpu_retrieval_time,
+            get_interval_emissions,
+            config_appearence,
+        )
         myApp.show()
-        print('\nPlot window in execution...')
+        print("\nPlot window in execution...")
         try:
             sys.exit(app.exec_())
         except SystemExit:
-            print('Closing Window...')
+            print("Closing Window...")
     except Exception as e:
-        print(f"Sorry, an error occurred: "
+        print(
+            f"Sorry, an error occurred: "
             f"\n{str(e)}"
             f"\nIf the error persists, report it to the developers opening an issue on github:"
-            f"\nhttps://github.com/MattiaSilvestri/energy-monitor/issues" 
-            )
+            f"\nhttps://github.com/MattiaSilvestri/energy-monitor/issues"
+        )
 
 
-def combine_cpu_CO2(cpu_usage: float, usage_time: float, 
-                    cpu_tdp: float, co2_intensity: int) -> float:
+def combine_cpu_CO2(
+    cpu_usage: float, usage_time: float, cpu_tdp: float, co2_intensity: int
+) -> float:
     """
     Combine CPU usage, CPU TDP and CO2 intensity to compute CO2 consumption.
 
@@ -86,19 +104,22 @@ def combine_cpu_CO2(cpu_usage: float, usage_time: float,
     """
 
     # transfrom Watt in kWatt
-    cpu_tdp_KW = cpu_tdp/1000
+    cpu_tdp_KW = cpu_tdp / 1000
 
     # transfrom kW in kWh and take a certain proportion of it
-    used_KWh = cpu_usage * cpu_tdp_KW / 100 * (usage_time/3600)
+    used_KWh = cpu_usage * cpu_tdp_KW / 100 * (usage_time / 3600)
 
     # get the g of Co2
-    co2_emissions = round(used_KWh * co2_intensity, config["Functional"]["co2_emissions_precision"])
+    co2_emissions = round(
+        used_KWh * co2_intensity, config["Functional"]["co2_emissions_precision"]
+    )
 
     return co2_emissions
 
 
-def get_interval_emissions(cpu_tdp: float, co2_intensity: int, 
-                           time_frequency: int) -> float:
+def get_interval_emissions(
+    cpu_tdp: float, co2_intensity: int, time_frequency: int
+) -> float:
     """
     From a given cpu_tdp and co2_intensity return the co2 emissions computing the cpu_usage at current time.
 
@@ -113,7 +134,9 @@ def get_interval_emissions(cpu_tdp: float, co2_intensity: int,
     """
 
     # Retrieve cpu usage
-    cpu_usage = cpu.get_cpu_usage(time_frequency-config["Functional"]["cpu_retrieval_time_decrement"])
+    cpu_usage = cpu.get_cpu_usage(
+        time_frequency - config["Functional"]["cpu_retrieval_time_decrement"]
+    )
 
     # Combine Co2 with CPU usage
     co2_emissions = combine_cpu_CO2(cpu_usage, time_frequency, cpu_tdp, co2_intensity)
